680e340a02b25271b2c477c40f0e9a26d1301aa2
Matheus Rezende <matheus.rezende@aircall.io>
2021-06-07 16:25:26 +0200

    fix script

--- install.sh
+++ install.sh
@@ -22,28 +22,26 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Create symlink to zshrc
-rm -rf $HOME/.zshrc
-
-ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 
 # Tmuxinator completions for ZSH
 wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O /usr/local/share/zsh/site-functions/_tmuxinator
 
 # Create symlink to tmux config
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 
-# change zsh to default shell
-chsh -s $(which zsh)
 
 # change vim to nvim
-ln -s /usr/local/bin/nvim /usr/local/bin/vim
+ln -sf /usr/local/bin/nvim /usr/local/bin/vim
 
 # Load neovim configurations
-ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 
 # Create symlink to tmux config
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 
-nvm install stable
+
+source ~/.zshrc
+nvm install 'lts/*' && nvm alias default 'lts/*'
 
 
